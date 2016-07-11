e560f7b1e4e55d3c7eefe863161166b36e704754
Rencheng Chua <rench00@gmail.com>
2016-07-11 13:33:58 +0800

    display pieces on html based on script array

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
   var Reversi = function (){
-    this.boardArr = [[1, 0, 0, 0, 0, 0, 0, 1],
+    this.boardArr = [[0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 0, 0, 0],
-                    [0, 0, 0, 0, 0, -1, 0, 0],
-                    [0, 0, 0, 0, 0, 0, -1, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 1],
-                    [1, 0, 0, 1, 0, 0, 0, 0]];
+                    [0, 0, 0, 1, -1, 0, 0, 0],
+                    [0, 0, 0, -1, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0]];
 
     this.playerTurnNow = 1; //keeps track of which player's turn it is now
 
     this.notPlayerTurnNow = -1; //keeps track of who the player who isn't playing now
 
-    this.positionPlaced = [3, 4]; //keeps track of the position that was selected
+    this.positionPlaced = [0, 0]; //keeps track of the position that was selected
 
     // keeps track of the position of pieces that need to change based on the position that was selected
     this.toChangeRightArr = [];
@@ -28,6 +28,10 @@ $(document).ready(function(){
   }; //end of prototype
 
   Reversi.prototype.play = function(){
+
+    // declaring variables needed;
+    var boundBoardArr = this.boardArr;
+    var positionPlacedArr = this.positionPlaced;
 
     var drawBoard = function(){
       // creates the squares
@@ -60,13 +64,61 @@ $(document).ready(function(){
 
     drawBoard();
 
-    boundBoardArr = this.boardArr
+    // display pieces
 
-    $(".space").on("click", function(){
+    var displayPieces = function(){
+      var black = "./assets/black.png";
+      var white = "./assets/white.png";
+      var spaceToFill = "";
+      for (i = 0; i < 8; i ++){
+        for (j = 0; j < 8; j ++){
+          if(this.boardArr[i][j] === -1){
+            spaceToFill = 'sp' + i + j;
+            $("#" + spaceToFill).append($("<img>", {class: 'piece', src: black}));
+          }
+          if(this.boardArr[i][j] === 1){
+            spaceToFill = 'sp' + i + j;
+            $("#" + spaceToFill).append($("<img>", {class: 'piece', src: white}));
+          }
+        }
+      }
+    }.bind(this);
+
+    displayPieces();
+
+    // check which position has been selected by player, check if anything needs to change, update board display
+    $(".space").click(function(){
       rowNum = parseInt(this.id.charAt(2));
       colNum = parseInt(this.id.charAt(3));
-      console.log(boundBoardArr[rowNum][colNum]);
-      });
+      positionPlacedArr = [rowNum, colNum];
+      console.log(positionPlacedArr);
+      checkChangeRight(positionPlacedArr);
+      return positionPlacedArr;
+    });
+
+    // check whether the row right of the position selected needs to be changed
+
+    var checkChangeRight = function(positionPlacedArr){
+      rowNum = positionPlacedArr[0];
+      colNum = positionPlacedArr[1];
+      for(i = colNum; i < this.boardArr.length - 1; i++){
+        if (this.boardArr[rowNum][i+1] === 0){
+          return [];
+        }else if ( this.boardArr[rowNum][i+1] === -1 ){
+          this.toChangeRightArr.push(rowNum, i+1);
+        }else if ( this.boardArr[rowNum][i+1] === 1){
+          return;
+        }
+      }
+      if(i === 7){
+        this.toChangeRightArr = [];
+      }
+    }.bind(this);
+
+    $(".test-btn").click(function(){
+      displayPieces();
+    }.bind(this));
+
 
   }; //end of prototype play function
 
